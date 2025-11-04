fcec99f842bfbe0839e100b8ad53e9f81dd04f7b
aryaayy <aryayyy9604@gmail.com>
2025-11-05 03:01:58 +0700

    minor changes

--- ProductService/function_app.py
+++ ProductService/function_app.py
@@ -8,7 +8,6 @@ from utils.auth import require_role, require_user, error
 
 app = func.FunctionApp()
 
-# ---------------- PUBLIC PAGES ----------------
 @app.route(route="product/products", auth_level=func.AuthLevel.ANONYMOUS)
 def get_products(req: func.HttpRequest) -> func.HttpResponse:
     with open("products_db.json", "r") as f:
@@ -33,7 +32,7 @@ def create_product(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         return func.HttpResponse(f"Bad Request: {e}", status_code=400)
 
-    new_product["id"] = f"P{(len(data["products"]) + 1):03d}"
+    new_product["product_id"] = f"P{(len(data["products"]) + 1):03d}"
     data["products"].append(new_product)
 
     with open("products_db.json", "w") as f:
@@ -60,7 +59,7 @@ def update_product(req: func.HttpRequest) -> func.HttpResponse:
 
     i = 0
     for row in data["products"]:
-        if row["id"] == updated_product["id"]:
+        if row["product_id"] == updated_product["product_id"]:
             break
         i += 1
     
@@ -91,14 +90,13 @@ def delete_product(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         return func.HttpResponse(f"Bad Request: {e}", status_code=400)
         
-    data["products"] = [row for row in data["products"] if row["id"] != body["id"]]
+    data["products"] = [row for row in data["products"] if row["product_id"] != body["product_id"]]
 
     with open("products_db.json", "w") as f:
         json.dump(data, f, indent=4)
 
-    return func.HttpResponse(f"Successfully deleted {body["id"]}", status_code=200, mimetype="application/json")
+    return func.HttpResponse(f"Successfully deleted {body["product_id"]}", status_code=200, mimetype="application/json")
 
-# Product management (Owner only)
 @app.route(route="product/manage", methods=["GET"], auth_level=func.AuthLevel.FUNCTION)
 def manage_products(req: func.HttpRequest) -> func.HttpResponse:
     claims, err = require_user(req)
