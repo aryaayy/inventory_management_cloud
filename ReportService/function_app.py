50135fea454fe5793b5e84734acafa148c5ce329
aryaayy <aryayyy9604@gmail.com>
2025-11-05 13:29:38 +0700

    report service

--- ReportService/function_app.py
+++ ReportService/function_app.py
@@ -4,63 +4,70 @@ import json
 import logging
 import requests
 import os
-import json
+import json, os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.auth import require_role, require_user, error
 
 app = func.FunctionApp()
 
 # Ambil token dan path dari environment (local.settings.json)
 TOKEN = os.getenv("AUTH_TOKEN")
-PRODUCTS_DB_PATH = os.getenv("REPORT_DB_PATH")
+INVENTORIES_DB_PATH = os.getenv("REPORT_DB_PATH")
 
-def load_products():
+def load_inventories(req: func.HttpRequest):
     try:
-        with open(PRODUCTS_DB_PATH, "r") as f:
-            data = json.load(f)
-        return data.get("products", []), None
+        data = requests.get(
+            "http://localhost:7073/api/inventory",
+            headers={
+                "Authorization": req.headers.get("Authorization")
+            }
+        )
+        return data.json(), None
     except Exception as e:
         return None, str(e)
 
-def generate_product_report(products):
-    if not products:
-        return {"error": "No products found"}
-    total_products = len(products)
-    total_value = sum(p.get("price", 0) * p.get("stock", 1) for p in products)
+def generate_inventory_report(inventories):
+    if not inventories:
+        return {"error": "No inventories found"}
+    total_inventories = len(inventories)
+    total_value = sum(p["product"]["price"] * p["available_qty"] for p in inventories)
 
     report_lines = [
-        f"=== Scheduled Product Report ===",
+        f"=== Scheduled Inventory Report ===",
         f"Generated at: {datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')} UTC",
-        f"Total products: {total_products}",
+        f"Total inventories: {total_inventories}",
         f"Total inventory value: {total_value:,} IDR",
         "Top products:"
     ]
 
-    for i, p in enumerate(products[:5], start=1):
-        report_lines.append(f"{i}. {p.get('name')} - Price: {p.get('price'):,} IDR, Stock: {p.get('stock')}")
+    for i, p in enumerate(inventories[:5], start=1):
+        report_lines.append(f"{i}. {p['product']['name']} - Price: {p['product']['price']:,} IDR, Available Stock: {p['available_qty']}, Sold: {p['sold_qty']}, In Process {p['reserved_qty']}")
 
-    if total_products > 5:
-        report_lines.append(f"... and {total_products - 5} more products")
+    if total_inventories > 5:
+        report_lines.append(f"... and {total_inventories - 5} more products")
 
     return "\n".join(report_lines)
 
-@app.schedule(schedule="0 */1 * * * *", arg_name="timer", run_on_startup=True, use_monitor=False)
-def scheduled_report(timer: func.TimerRequest) -> None:
-    try:
-        products, err = load_products()
-        if err:
-            logging.error(f"Failed to load products: {err}")
-            return
-        report = generate_product_report(products)
-        logging.info(report)
-    except Exception as e:
-        logging.error(f"Scheduled report error: {e}")
+# @app.schedule(schedule="0 */1 * * * *", arg_name="timer", run_on_startup=True, use_monitor=False)
+# def scheduled_report(timer: func.TimerRequest) -> None:
+#     try:
+#         inventories, err = load_inventories()
+#         if err:
+#             logging.error(f"Failed to load inventories: {err}")
+#             return
+#         report = generate_inventory_report(inventories)
+#         logging.info(report)
+#     except Exception as e:
+#         logging.error(f"Scheduled report error: {e}")
 
 @app.route(route="report/run", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def run_report(req: func.HttpRequest) -> func.HttpResponse:
-    token_header = req.headers.get("Authorization")
-    if not token_header or token_header.replace("Bearer ", "") != TOKEN:
-        return func.HttpResponse("Unauthorized", status_code=401)
-    products, err = load_products()
+    claims, err = require_user(req)
+    if err: 
+        return err
+    
+    inventories, err = load_inventories(req)
     if err:
-        return func.HttpResponse(f"Failed to load products: {err}", status_code=500)
-    report = generate_product_report(products)
+        return func.HttpResponse(f"Failed to load inventories: {err}", status_code=500)
+    report = generate_inventory_report(inventories)
     return func.HttpResponse(json.dumps(report), mimetype="application/json", status_code=200)
